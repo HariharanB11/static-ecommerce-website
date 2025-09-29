008519fdeeee6920a1b0b8fd4a863f3013446288
Hariharan Balaji <120601046+HariharanB11@users.noreply.github.com>
2025-09-29 16:27:39 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,8 +1,12 @@
 output "app_instance_public_ip" {
-  value = module.ec2_app.aws_instance.this.public_ip
+  value = module.ec2_app.public_ip
 }
 
 output "app_instance_id" {
-  value = module.ec2_app.aws_instance.this.id
+  value = module.ec2_app.instance_id
+}
+
+output "ecr_repository_url" {
+  value = module.ecr.repository_url
 }
 
